03dd6e5bbb11c006db268d6530e4cfd51d73f7f8
Mamed766 <mehemmeddemirli57@gmail.com>
2024-12-06 17:33:21 +0400

    bug fix

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -16,7 +16,7 @@ import {
 router.route("/").post(registerUser).get(getUsers);
 router.post("/logout", logoutUser);
 router.post("/login", authUser);
-router.route("/profile").get(getUserProfile).put(updateUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
 router.route("/:id").delete(deleteUser).get(getUserById).put(updateUser);
 
 export default router;
